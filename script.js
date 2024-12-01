f95faeabc34b859a9c03e845c07ab2014d73c2cc
YOUR_NAME <YOUR_EMAIL>
2024-12-01 15:18:48 +0300

    continuation_of_the_project

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-// "use strict";
+"use strict";
 
 
-// let id = document.querySelector("#dt");
+let id = document.querySelector("#dt");
 
-// let text = id.innerHTML;
+let text = id.innerHTML;
 
-// let i = 1;
-// // console.log(id);
-// // console.log(text);
+let i = 1;
+// console.log(id);
+// console.log(text);
 
-// window.addEventListener("load", animText);
+window.addEventListener("load", animText);
 
-// function animText(){
-//     id.innerHTML = text.substring(0, i);
-//     i ++;
-//     if (i > text.length){
-//         i = 0;
-//     }
-//     setTimeout(animText, 50);
-// }
+function animText(){
+    id.innerHTML = text.substring(0, i);
+    i ++;
+    if (i > text.length){
+        i = 0;
+    }
+    setTimeout(animText, 75);
+}
  
 // const el = document.getElementById("div-03");
 
 // // the closest ancestor with the id of "div-02"
 // console.log(el.closest("#div-02")); // <div id="div-02">
+
+// // the closest ancestor which is a div in a div
+// console.log(el.closest("div div")); // <div id="div-03">
 
 // // the closest ancestor which is a div and has a parent article
 // console.log(el.closest("article > div")); // <div id="div-01">
